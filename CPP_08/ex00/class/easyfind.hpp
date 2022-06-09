0bcb7a56e8a3cbf926a73bedc260fc6f2b3718b2
gsap <gaetan.sap@gmail.com>
2022-06-09 11:38:59 +0200

    ex00 done

---
+++ CPP_08/ex00/class/easyfind.hpp
@@ -0,0 +1,33 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <string>
+# include <iostream>
+# include <iomanip>
+# include <algorithm>
+# include <vector>
+
+class ElementNotFound : public std::exception
+{
+	public:
+		virtual const char* what() const throw() {
+				return ("\e[91mThe element is not in this array !\e[39m");
+		}
+};
+
+template< typename T >
+int	easyfind(T cont, int x) {
+	int size = cont.size();
+	for (int i = 0; i < size; i++)
+		if (cont[i] == x)
+			return (i);
+	throw ElementNotFound();
+	return (0);
+}
+
+template< typename T >
+void	DispVector(T tab, int size) {
+	for (int i = 0; i < size; i++)
+		std::cout << "tab[" << i << "] => " << tab[i] << std::endl;
+}
+#endif
