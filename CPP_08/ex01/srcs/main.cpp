816f6e4eb31c2d32bc7973d2fe5f4e9491e684f9
gsap <gaetan.sap@gmail.com>
2022-06-09 16:01:42 +0200

    pk ca marche plus

---
+++ CPP_08/ex01/srcs/main.cpp
@@ -0,0 +1,118 @@
+# include "../class/Span.hpp"
+
+int main( void )
+{
+	std::cout << std::endl;
+	std::cout << "\t\t\e[92m---MAIN---\e[39m" << std::endl << std::endl;
+	{
+		std::cout << std::endl;
+		std::cout << "\t\e[91m---PROVIDED-MAIN---\e[39m" << std::endl << std::endl;
+		{
+			Span sp = Span(5);
+			sp.addNumber(6);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		}
+		std::cout << std::endl;
+		std::cout << "\t\e[93m---PROVIDED-MAIN---\e[39m" << std::endl << std::endl;
+		
+		std::cout << std::endl;
+		std::cout << "\t\e[91m---CUSTOM-MAIN---\e[39m" << std::endl << std::endl;
+		{
+			std::cout << std::endl;
+			std::cout << "\e[96m*To much add*\e[39m" << std::endl << std::endl;
+			{
+				try
+				{
+					Span	sp = Span(2);
+					
+					sp.addNumber(0);
+					sp.addNumber(1);
+					sp.addNumber(2);
+					sp.addNumber(3);
+				}
+				catch (const std::exception& e)
+				{ std::cerr << e.what() << std::endl; }
+			}
+			
+			std::cout << std::endl;
+			std::cout << "\e[96m*Span empty*\e[39m" << std::endl << std::endl;
+			{
+				std::cout << "shortestSpan" << std::endl;
+				{
+					try
+					{
+						Span	sp = Span(0);
+						
+						sp.shortestSpan();
+					}
+					catch (const std::exception& e)
+					{ std::cerr << e.what() << std::endl; }
+				}
+				
+				std::cout << std::endl;
+				std::cout << "longestSpan" << std::endl;
+				{
+					try
+					{
+						Span	sp = Span(0);
+						
+						sp.longestSpan();
+					}
+					catch (const std::exception& e)
+					{ std::cerr << e.what() << std::endl; }
+				}
+			}
+			
+			std::cout << std::endl;
+			std::cout << "\e[96m*Span size 1*\e[39m" << std::endl << std::endl;
+			{
+				std::cout << "shortestSpan" << std::endl;
+				{
+					try
+					{
+						Span	sp = Span(1);
+						
+						sp.addNumber(0);
+						sp.shortestSpan();
+					}
+					catch (const std::exception& e)
+					{ std::cerr << e.what() << std::endl; }
+				}
+				
+				std::cout << std::endl;
+				std::cout << "longestSpan" << std::endl;
+				{
+					try
+					{
+						Span	sp = Span(1);
+						
+						sp.addNumber(0);
+						sp.longestSpan();
+					}
+					catch (const std::exception& e)
+					{ std::cerr << e.what() << std::endl; }
+				}
+			}
+			
+			// std::cout << std::endl;
+			// std::cout << "\e[96m*Span size 10,000*\e[39m" << std::endl << std::endl;
+			// {
+			// 	Span	sp = Span(10000);
+			// 
+			// 	sp.addRandom(10000 - 1);
+			// 	std::cout << "shortestSpan: " << sp.shortestSpan() << std::endl;
+			// 	std::cout << "longestSpan: " << sp.longestSpan() << std::endl;
+			// }
+		}
+		std::cout << std::endl;
+		std::cout << "\t\e[93m---PROVIDED-MAIN---\e[39m" << std::endl << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << "\t\t\e[93m---MAIN---\e[39m" << std::endl << std::endl;
+	return (0);
+}
