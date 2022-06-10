6e620e5296fffa993cf7e2f252f84f6cc04f0f96
gsap <gaetan.sap@gmail.com>
2022-06-10 10:25:35 +0200

    ex02 begin

--- CPP_08/ex01/srcs/main.cpp
+++ CPP_08/ex01/srcs/main.cpp
@@ -143,7 +143,7 @@ int main( void )
 			}
 		}
 		std::cout << std::endl;
-		std::cout << "\t\e[93m---PROVIDED-MAIN---\e[39m" << std::endl << std::endl;
+		std::cout << "\t\e[93m---CUSTOM-MAIN---\e[39m" << std::endl << std::endl;
 	}
 	std::cout << std::endl;
 	std::cout << "\t\t\e[93m---MAIN---\e[39m" << std::endl << std::endl;
---
+++ CPP_08/ex02/class/MutantStack.hpp
@@ -0,0 +1,15 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <string>
+# include <iostream>
+# include <iomanip>
+# include <algorithm>
+# include <vector>
+
+class Span
+{
+	
+};
+
+#endif
