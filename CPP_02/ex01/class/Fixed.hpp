46bca2811f24c316d28937f37b4ecb56f9f34eff
gsap <gaetan.sap@gmail.com>
2022-06-10 14:45:21 +0200

    ex01 rework

--- CPP_02/ex01/class/Fixed.hpp
+++ CPP_02/ex01/class/Fixed.hpp
@@ -7,26 +7,29 @@
 
 class Fixed
 {
+	private:
+		int					m_value;
+		static const int	m_bits = 8;
+
 	public:
 		//Orthodox Canonical Form
 		Fixed();
 		~Fixed();
 		Fixed(Fixed const& src);
 		Fixed & operator=(Fixed const& src);
+
 		//Constructor
 		Fixed(const int nb);
 		Fixed(const float nb);
+
 		//Functions
 		float	toFloat(void) const;
 		int		toInt(void) const;
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
-	
-	private:
-		int		m_value;
-		static const int	m_bits = 8;
 };
 
+//Display
 std::ostream & operator << (std::ostream &out, const Fixed &c);
 
 #endif
--- CPP_02/ex01/srcs/Fixed.cpp
+++ CPP_02/ex01/srcs/Fixed.cpp
@@ -1,44 +1,65 @@
 #include "../class/Fixed.hpp"
 
-Fixed::Fixed()
-{ std::cout << "\e[92m---Default Constructor Call---\e[39m" << std::endl; }
+//					//
+//	Canonical Form	//
+//					//
 
-Fixed::~Fixed()
-{ std::cout << "\e[92m---Destructor Call---\e[39m" << std::endl; }
+Fixed::Fixed(): m_value(0) {
+	std::cout << "\e[92m---Default Constructor Call---\e[39m" << std::endl;
+}
 
-Fixed::Fixed(Fixed const& src): m_value(src.m_value)
-{ std::cout << "\e[92m---Copy Constructor Call---\e[39m" << std::endl; }
+Fixed::~Fixed() {
+	std::cout << "\e[92m---Destructor Call---\e[39m" << std::endl;
+}
 
-Fixed & Fixed::operator=(Fixed const& src)
-{
+Fixed::Fixed(Fixed const& src): m_value(src.m_value) {
+	std::cout << "\e[92m---Copy Constructor Call---\e[39m" << std::endl;
+}
+
+Fixed & Fixed::operator=(Fixed const& src) {
 	std::cout << "\e[92m---Assignement Constructor Call---\e[39m" << std::endl;
-	m_value = src.m_value;
+	this->m_value = src.m_value;
 	return (*this);
 }
 
-Fixed::Fixed(const int nb)
-{
+//					//
+//	Constructors	//
+//					//
+
+Fixed::Fixed(const int nb) {
 	std::cout << "\e[92m---Int Constructor Call---\e[39m" << std::endl;
 	this->m_value = (nb << Fixed::m_bits);
 }
 
-Fixed::Fixed(const float nb)
-{
+Fixed::Fixed(const float nb) {
 	std::cout << "\e[92m---Float Constructor Call---\e[39m" << std::endl;
 	this->m_value = roundf(nb * (1 << Fixed::m_bits));
 }
 
-int		Fixed::getRawBits() const
-{ return (m_value); }
+//				//
+//	Functions	//
+//				//
 
-void	Fixed::setRawBits(int const raw)
-{ m_value = raw; }
+int		Fixed::getRawBits() const {
+	return (this->m_value);
+}
 
-float Fixed::toFloat(void) const
-{ return ((float)this->m_value / (float)(1 << Fixed::m_bits)); }
+void	Fixed::setRawBits(int const raw) {
+	this->m_value = raw;
+}
 
-int Fixed::toInt(void) const
-{ return (this->m_value >> Fixed::m_bits); }
+float Fixed::toFloat(void) const {
+	return ((float)this->m_value / (float)(1 << Fixed::m_bits));
+}
 
-std::ostream & operator << (std::ostream &out, const Fixed &c)
-{ return (out << c.toFloat()); }
+int Fixed::toInt(void) const {
+	return (this->m_value >> Fixed::m_bits);
+}
+
+//			//
+//	Display	//
+//			//
+
+std::ostream & operator << (std::ostream &out, const Fixed &c) {
+	return (out << c.toFloat());
+}
