fc84490461a5def8b48d6560958a0741c85d0284
gsap <gaetan.sap@gmail.com>
2022-06-10 13:32:28 +0200

    Mutant Done

--- CPP_08/ex02/class/MutantStack.hpp
+++ CPP_08/ex02/class/MutantStack.hpp
@@ -6,10 +6,37 @@
 # include <iomanip>
 # include <algorithm>
 # include <vector>
+# include <stack>
+# include <list>
 
-class Span
+template< typename T >
+class MutantStack: public std::stack<T>
 {
-	
+	public:
+		//Constructors
+		MutantStack(): std::stack<T>() {}
+		~MutantStack() {}
+		MutantStack(MutantStack const& src) { this->c = src.c; }
+		MutantStack & operator=(MutantStack const& src) { 
+			this->c = src.c;
+			return (*this);
+		}
+		
+		//Typedef
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+		
+		//Functions
+		iterator				begin() { return (this->c.begin()); }
+		iterator				end() { return (this->c.end()); }
+		const_iterator			cbegin() { return (this->c.cbegin()); }
+		const_iterator			cend() { return (this->c.cend()); }
+		reverse_iterator		rbegin() { return (this->c.rbegin()); }
+		reverse_iterator		rend() { return (this->c.rend()); }
+		const_reverse_iterator	crbegin() { return (this->c.crbegin()); }
+		const_reverse_iterator	crend() { return (this->c.crend()); }
 };
 
 #endif
--- CPP_08/ex02/srcs/main.cpp
+++ CPP_08/ex02/srcs/main.cpp
@@ -5,7 +5,125 @@ int main( void )
 	std::cout << std::endl;
 	std::cout << "\t\t\e[92m---MAIN---\e[39m" << std::endl << std::endl;
 	{
+		std::cout << std::endl;
+		std::cout << "\t\e[91m---PROVIDED-MAIN---\e[39m" << std::endl << std::endl;
+		{
+			std::cout << std::endl;
+			std::cout << "\e[96m*MutantStack*\e[39m" << std::endl << std::endl;
+			{
+				MutantStack<int> mstack;
+				
+				mstack.push(5);
+				mstack.push(17);
+				std::cout << mstack.top() << std::endl;
+				mstack.pop();
+				std::cout << mstack.size() << std::endl;
+				mstack.push(3);
+				mstack.push(5);
+				mstack.push(737);
+				//[...]
+				mstack.push(0);
+				MutantStack<int>::iterator it = mstack.begin();
+				MutantStack<int>::iterator ite = mstack.end();
+				++it;
+				--it;
+				while (it != ite)
+				{
+					std::cout << *it << std::endl;
+					++it;
+				}
+				std::stack<int> s(mstack);
+			}
+		}
+		std::cout << std::endl;
+		std::cout << "\t\e[93m---PROVIDED-MAIN---\e[39m" << std::endl << std::endl;
 		
+		std::cout << std::endl;
+		std::cout << "\t\e[91m---CUSTOM-MAIN---\e[39m" << std::endl << std::endl;
+		{
+			std::cout << std::endl;
+			std::cout << "\e[96m*std::list*\e[39m" << std::endl << std::endl;
+			{
+				std::list<int> mstack;
+				
+				mstack.push_back(5);
+				mstack.push_back(17);
+				std::cout << mstack.front() << std::endl;
+				mstack.pop_back();
+				std::cout << mstack.size() << std::endl;
+				mstack.push_back(3);
+				mstack.push_back(5);
+				mstack.push_back(737);
+				//[...]
+				mstack.push_back(0);
+				std::list<int>::iterator it = mstack.begin();
+				std::list<int>::iterator ite = mstack.end();
+				++it;
+				--it;
+				while (it != ite)
+				{
+					std::cout << *it << std::endl;
+					++it;
+				}
+				std::list<int> s(mstack);
+			}
+			
+			std::cout << std::endl;
+			std::cout << "\e[96m*std::vector*\e[39m" << std::endl << std::endl;
+			{
+				std::vector<int> mstack;
+				
+				mstack.push_back(5);
+				mstack.push_back(17);
+				std::cout << mstack.front() << std::endl;
+				mstack.pop_back();
+				std::cout << mstack.size() << std::endl;
+				mstack.push_back(3);
+				mstack.push_back(5);
+				mstack.push_back(737);
+				//[...]
+				mstack.push_back(0);
+				std::vector<int>::iterator it = mstack.begin();
+				std::vector<int>::iterator ite = mstack.end();
+				++it;
+				--it;
+				while (it != ite)
+				{
+					std::cout << *it << std::endl;
+					++it;
+				}
+				std::vector<int> s(mstack);
+			}
+			
+			std::cout << std::endl;
+			std::cout << "\e[96m*std::deque*\e[39m" << std::endl << std::endl;
+			{
+				std::deque<int> mstack;
+				
+				mstack.push_back(5);
+				mstack.push_back(17);
+				std::cout << mstack.front() << std::endl;
+				mstack.pop_back();
+				std::cout << mstack.size() << std::endl;
+				mstack.push_back(3);
+				mstack.push_back(5);
+				mstack.push_back(737);
+				//[...]
+				mstack.push_back(0);
+				std::deque<int>::iterator it = mstack.begin();
+				std::deque<int>::iterator ite = mstack.end();
+				++it;
+				--it;
+				while (it != ite)
+				{
+					std::cout << *it << std::endl;
+					++it;
+				}
+				std::deque<int> s(mstack);
+			}
+		}
+		std::cout << std::endl;
+		std::cout << "\t\e[93m---CUSTOM-MAIN---\e[39m" << std::endl << std::endl;
 	}
 	std::cout << std::endl;
 	std::cout << "\t\t\e[93m---MAIN---\e[39m" << std::endl << std::endl;
